81de4a8d55e962a98a48d4eff328b3eefee6d868
Anthony R <ar664@njit.edu>
2015-04-15 15:32:19 -0400

    Brightness Comparison
    Added RGB * their respective wave values.

--- Bimp2Asc/Bimp2Asc/OpenFile.cpp
+++ Bimp2Asc/Bimp2Asc/OpenFile.cpp
@@ -9,14 +9,19 @@ using namespace std;
 #define WIDTH  550
 #define XBOX 7
 #define YBOX 10
-#define files 19
+#define filenum 14
+#define MAXBRIGHT 138975
 
-char filenames[files][6] = {"(.bmp",").bmp","O.bmp","!.bmp","#.bmp","$.bmp","%.bmp","&.bmp",";.bmp","~.bmp","+.bmp","`.bmp","}.bmp","{.bmp","_.bmp","^.bmp","].bmp","[.bmp", "@.bmp"};
+char filenames[filenum][6] = {"`.bmp","~.bmp","^.bmp","!.bmp",";.bmp","(.bmp","&.bmp","%.bmp","+.bmp","].bmp","$.bmp","O.bmp","#.bmp", "@.bmp"};
+int fileData[filenum];
+int bratio = 25;
 int cData[HEIGHT][WIDTH];
 
 char yes(int x){
 	if (x >= 254){
-		return '.';
+		return ' ';
+	}else if (x >= 233){
+		return '.'; 
 	}else if (x >= 204){
 		return 'o';
 	}else if (x >= 153){
@@ -29,57 +34,56 @@ char yes(int x){
 	
 }
 
-char compare(int m[XBOX][YBOX], int thresh_hold){
-	cout << "Comparing";
-	int tries = 0, pixels = 0;
-	int newBox[XBOX][YBOX];
-	for (int i = 0;  i < XBOX; i++){ //Sets an 2d array with 1 being darker than normal and 0 being lighter than normal
-		for(int j = 0; j < YBOX; j++){
-			if(m[i][j] <= thresh_hold){
-				//cout << "Current value: " << m[i][j] << " Threshold: " << thresh_hold;
-				newBox[i][j] = 1;
-				pixels++;
-			}else{
-				//cout << "Current value: " << m[i][j] << " Threshold: " << thresh_hold;
-				newBox[i][j] = 0;
-			}
+int lowest(int y[filenum]){
+	int z;
+	for(int i = 0; i < filenum; i++){
+		if (i == 0){
+			z = y[i];
+		} else if (y[i] < z){
+			z = y[i];
 		}
 	}
-	cout<< "Trying to match: " << pixels << " pixels..." << endl;
-	if (pixels == 0){
-		return ' ';
-	}
-	while (tries < 10){
-		for (int k = 0; k < files; k++){
-			CImg<unsigned char> image(filenames[k]);
-			int charBox[XBOX][YBOX];
-			int testBox = 0;
-			int filepix = 0;
-			for (int x = 0; x < image.width(); x++){ //creates a 2d array of character with pixels being 1 and no pixels being 0
-				for(int y = 0; y < image.height(); y++){
-					if (image(x,y,0,1) < 250){
-						//cout << "Pixel of" << filenames[k] <<  "at: " << x << ", " << y << endl;
-						charBox[x][y] = 1;
-						filepix++;
-					}else{
-						charBox[x][y] = 0;
-					}
-					if (newBox[x][y] == charBox[x][y]){
-						testBox++;
-					}
+	return z;
+}
+	
+void chatData(int *cBox){
+	//int fileRay[] = calloc(filenum*sizeof(int));
+	for (int k = 0; k < filenum; k++){
+		CImg<unsigned char> image(filenames[k]);
+		int charBox[XBOX][YBOX];
+		int testBox = 0;
+		int filepix = 0;
+		for (int x = 0; x < image.width(); x++){ //creates a 2d array of character with pixels being 1 and no pixels being 0
+			for(int y = 0; y < image.height(); y++){
+				if (image(x,y,0,1) < 250){
+					//cout << "Pixel of" << filenames[k] <<  "at: " << x << ", " << y << endl;
+					charBox[x][y] = 1;
+					filepix++;
+				}else{
+					charBox[x][y] = 0;
 				}
+
 			}
-			//cout << "Current file: " << filenames[k] << " Pixels(file): "<< filepix <<  endl;
-				
-			//cin.get();
-			//cout << "testBox: " << testBox << "Pixels: " << pixels << endl;
-			if (testBox == (pixels+tries)){
-				cout << "Found match: " << filenames[k][0] << endl;
-				return filenames[k][0];
-			}
+			
 		}
-		//cout << "Tries: " << tries << endl;
-		tries++;
+		//## Giving the value of cBox to compare with brightness later. ###
+		cBox[k] = MAXBRIGHT - MAXBRIGHT*((float) filepix/25);
+		//free(image);
+		cout << "Current file: " << filenames[k] << " Pixels(file): "<< filepix << " With brightness: "<< cBox[k] << endl;
+		//cBox = charBox;
+	}
+}
+
+//if (newBox[x][y] == charBox[x][y]){ Old matching code..
+	//testBox++;
+//}
+
+char compare(int Brightness){
+	//cout << "Comparing" << endl;
+	for(int  k = 0; k < filenum; k++){
+		if(fileData[k] <= Brightness){
+			return filenames[k][0];
+		}
 	}
 	return '.';
 }
@@ -90,7 +94,8 @@ int main(){
 	myfile.clear();
 	CImg<unsigned char> image("duck.bmp");
 	CImgDisplay display(image,"This is a duck");
-	cout << image._spectrum << endl;
+	int* filePnt = &fileData[0];
+	chatData(filePnt);
 	//image.RGBtoHSL();
 	//display.wait();
 	cout << "Height: " << image._height << " Width: " << image._width << endl;
@@ -110,13 +115,14 @@ int main(){
 	cout << "Second File: " << filenames[1] << endl;
 
 	cimg_forXY(image,x,y){
-		int Red =  image(x,y,0,0);
-		int Green = image(x,y,0,1);
-		int Blue = image(x,y,0,2);
-		int average = (Red+Green+Blue)/3;
-		cData[x][y] = average;
+		int Red =  image(x,y,0,0)*650;
+		int Green = image(x,y,0,1)*510;
+		int Blue = image(x,y,0,2)*475;
+		int brightness = (Red+Green+Blue)/3;
+		cData[x][y] = brightness;
 		//cout << "Average:" << average << endl;
 	}
+
 
 	for (int i = 0 ; i < newHieght-10; i+=10){
 		for (int j = 0; j < newWidth-7;j+=7){
@@ -131,9 +137,11 @@ int main(){
 					sum += currentBox[k][m];
 				}
 			}
-			int average = sum/(XBOX*YBOX) - 1;
-			cout << average;
-			myfile << compare(currentBox,average);		}
+			int avgbright = sum/(XBOX*YBOX) - 1;
+			//cout << " Box#: "<< (i+j) << " brightness:" << avgbright << endl;
+			//myfile << yes(avgbright);
+			myfile << compare(avgbright);		
+		}
 		myfile << endl;
 	}
 	myfile.close();
