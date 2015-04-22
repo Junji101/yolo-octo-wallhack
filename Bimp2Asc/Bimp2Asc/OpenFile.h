2f592d326c1c819e290f4b50fc65ecfaef65a3a8
Anthony R <ar664@njit.edu>
2015-04-22 19:07:04 -0400

    The Rest

---
+++ Bimp2Asc/Bimp2Asc/OpenFile.h
@@ -0,0 +1,34 @@
+#include "Headers\CImg.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <stdlib.h> 
+#include <exception>
+#include "Headers\inttypes.h"
+//#include "C:\Users\ANNIE\Desktop\ffmpeg\ffmpeg.h"
+//#define cimglist_plugin "cvMat.h"
+//#define inline __inline
+
+using namespace cimg_library;
+using namespace std;
+
+#define HEIGHT  700
+#define WIDTH  700
+#define XBOX 5
+#define YBOX 5
+#define fileNum 14
+#define MAXBRIGHT 138975
+#define MAXCHARACTERS 26624
+#define MAXFRAMES 900
+#define RESIZE false
+
+/*class ProcessImages{
+	int x;
+	int y;
+	string pattern;
+public:
+	void charData(int*) {};
+	void doTheThing(){};
+}
+*/
